9f77acc6defd3d0e2c239086e26df4d91a5ada40
“Risseii” <vanibel267@gmail.com>
2022-02-21 19:52:31 -0500

    Tarea

---
+++ megusta.js
@@ -0,0 +1,9 @@
+
+var click = 3;
+var click1 = document.querySelector("#numero");
+
+function increase(){
+    click ++;
+    click1.innerText = click + " Likes";
+  
+}
---
+++ megusta2.js
@@ -0,0 +1,26 @@
+var click = 9;
+var total = document.querySelector("#like1");
+
+function suma1(){
+    click ++;
+    total.innerText = click + " likes";
+
+}
+
+
+var click2 = 12;
+var total2 = document.querySelector("#like2");
+
+function suma2(){
+    click2++;
+    total2.innerText = click2 + " likes";
+}
+
+
+var click3 = 9;
+var total3 = document.querySelector("#like3");
+
+function suma3(){
+    click3++;
+    total3.innerText = click3 + " likes";
+}
